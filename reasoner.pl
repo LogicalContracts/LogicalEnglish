8eb1b58fc23d8cfd9f9fec11a91c9f096b015103
Miguel Calejo <mc@interprolog.com>
2021-01-11 18:08:54 +0000

    Explanations for negative goals (still untested)

--- reasoner.pl
+++ reasoner.pl
@@ -9,103 +9,172 @@
 
 :- use_module(kp_loader).
 
-i( at(G,KP),U,E) :- !,context_module(M), i(at(G,KP),M,U,E).
+% i(+AtGoal,-Unknowns,-Why)
+i( at(G,KP),U,E) :- !,context_module(M), i(at(G,KP),M,top_goal,U,E).
 i( _G,_,_) :- print_message(error,top_goal_must_be_at), fail.
 
-% i(+Goal,+LoadedModule,-Unknowns,-Why) 
-%  explanation is a proof tree: w(nodeLiteral,childrenNodes); [] denotes.. self-evident; 
-%TODO: negative explanations 
+% i(+Goal,+AlreadyLoadedModule,+CallerGoalID,-Unknowns,-Why) 
+%  explanation is a proof tree: w(nodeLiteral,ClauseRef,childrenNodes); [] denotes.. self-evident; 
+%  if nextGoalID(ID), i(G,...) fails with zero solutions, there will be a failed tree asserted with root failed(ID,...)
+%  failures for not(G) goals will also leave asserted a fact failed_success(ID,Why)
+%   successes for not(G) goals will have a Why with f(NegatedGoalID,CallerID,FreeVar)
+%  there may be orphan failed(ID,...) facts, because we're focusing on solution-less failures only
+%  this predicate is NOT thread safe
 %TODO: add meta_predicate(i(0,...)) declarations...?
 % failure means false; success with empty Unknowns list means true; 
 % otherwise, result unknown, depending on solutions to goals in Unknowns; 
-% _-system is an "unknown" likely irrelevant, a consequence of others
-i(G,_,_,_) :- mylog(i-G), fail.
-i(G,M,_,_) :- var(G), !, throw(variable_call_at(M)).
-i(true, _, [], []) :- !.
-i(false, _, [], []) :- !, fail.
-i(and(A,B), M, U, E) :- !, i((A,B),M,U,E).
-i((A,B), M, U, E) :- !, i(A,M,U1,E1), i(B,M,U2,E2), append(U1,U2,U), append(E1,E2,E).
-i(or(A,B), M, U, E) :- !, i((A;B),M, U,E).
-i((A;B), M, U, E) :- !, (i(A,M,U,E) ; i(B,M,U,E)).
-i(must(I,M), Mod, U, E) :- !, i(then(I,M), Mod, U, E).
-i(not(G),M,U,E) :- !, i( \+ G,M,U,E).
-i(\+ G, M, [], [w(not(G),[])]) :- !, \+ i( G, M, [], _). % notice this requires no unknowns!
-i(!,_,_,_) :- throw(no_cuts_allowed).
-i(';'(C->T,Else), M, U, E) :- !, % forbid this?
-    ( i(C,M,UC,EC) -> 
-        (i(T,M,UT,ET), append(UC,UT,U), append(EC,ET,E))
-        ; (i(Else,M,U,EE), E = [w(not(C),[])|EE] )).
-i((If->Then),M,U,E) :- !, % forbid this?
-    (i(If,M,UI,EI) -> (i(Then,M,UT,ET), append(UI,UT,U), append(EI,ET,E))).
-i(then(if(C),else(T,Else)), M, U, E) :- !,
-    (   i(C,M,UC,EC), i(T,M,UT,ET), append(UC,UT,U), append(EC,ET,E) ; 
-        i( \+ C,M,UC,EC), i(Else,M,UE,EE), append(UC,UE,U), append(EC,EE,E) ).
-i(then(if(C),Then),M,U,E) :- !, i(then(if(C),else(Then,true)),M,U,E).
-i(forall(A,B),M,U,[w(forall(A,B),Children)]) :- !, 
-    findall(X, (i(A,M,UA,EA), (i(B,M,UB,EB) -> (append(UA,UB,Ui),append(EA,EB,Ei),X=Ui/Ei) ; X=failed)), Tuples),
-    \+ member(failed,Tuples),
-    findall(Ui,member(Ui/_,Tuples),U_), append(U_,U),
-    findall(Ei,member(_/Ei,Tuples),Children_), append(Children_,Children).
-i(setof(X,G,L),M,U,[w(setof(X,G,L),Children)]) :- !, 
-    wrapTemplateGoal(G,M,Ui,Ei,Wrapped),
+%TODO: ?? _-system is an "unknown" likely irrelevant, a consequence of others
+% i(G,_,_,_) :- mylog(i-G), fail.
+i(G,M,_,_,_) :- var(G), !, throw(variable_call_at(M)).
+i(true, _, _, U, E) :- !, U=[], E=[].
+i(false, _, _, _U, _E) :- !, fail.
+i(and(A,B), M, CID, U, E) :- !, i((A,B),M,CID,U,E).
+i((A,B), M, CID, U, E) :- !, i(A,M,CID,U1,E1), i(B,M,CID,U2,E2), append(U1,U2,U), append(E1,E2,E).
+i(or(A,B), M, CID, U, E) :- !, i((A;B),M, CID,U,E).
+i((A;B), M, CID, U, E) :- !, (i(A,M,CID,U,E) ; i(B,M,CID,U,E)).
+i(must(I,M), Mod, CID, U, E) :- !, i(then(I,M), Mod, CID, U, E).
+i(\+ G,M,CID, U,E) :- !, i( not(G),M,CID, U,E).
+i(not(G), M, CID, U, NotE) :- !, 
+    U=[],
+    newGoalID(NotID),
+    % our negation as failure requires no unknowns:
+    ( i( G, M, NotID, [], E) -> (
+        assert( failed(NotID,CID,not(G))),
+        assert( failed_success(NotID,E)),
+        fail
+    ) ; (
+        NotE = [f(NotID,CID,_NotHere_TheyAreAsserted)]
+    )).
+i(!,_,_,_,_) :- throw(no_cuts_allowed).
+i(';'(C->T,Else), M, CID, U, E) :- !,% should we forbid Prolog if-then-elses..?
+    nextGoalID(ID),
+    ( i(C,M,CID, UC,EC) -> (
+        i(T,M,CID,UT,ET), 
+        append(UC,UT,U), append(EC,ET,E)
+    ) ; (
+        % any further failures under the (failed) condition?
+        ((nextGoalID(Other), Other\=ID) -> EC=[f(ID,CID,_NotYet)] ; EC=[]),
+        i(Else,M,CID,U,EE), append(EC,EE,E) 
+    )).
+i((If->Then),M,CID,U,E) :- !, 
+    i((If->Then;fail),M,CID,U,E).
+i(then(if(C),else(T,Else)), M, CID, U, E) :- !,
+    nextGoalID(ID),
+    (i(C,M,CID,UC,EC) *-> (
+        i(T,M,CID,UT,ET), 
+        append(UC,UT,U), append(EC,ET,E)
+    ) ; (
+        ((nextGoalID(Other), Other\=ID) -> EC=[f(ID,CID,_NotYet)] ; EC=[]),
+        i(Else,M,CID,U,EE), % no unknowns under C for sure
+        append(EC,EE,E)
+    )).
+i(then(if(C),Then),M,CID,U,E) :- !, i(then(if(C),else(Then,true)),M,CID,U,E).
+i(forall(A,B),M,CID,U,E) :- !, E=[w(forall(A,B),Children)],
+    newGoalID(ForID),
+    findall(X, (
+        i(A,M,ForID,UA,EA), 
+        nextGoalID(ID),
+        (i(B,M,ForID,UB,EB) -> (
+            append(UA,UB,Ui),append(EA,EB,Ei),X=Ui/Ei
+        ) ; (
+            % failed; was there a relevant failure under B?
+            ((nextGoalID(Other), Other\=ID) -> EB=[f(ID,CID,_NotYet)] ; EB=[]),
+            append(EA,EB,Ei),
+            X=failed(UB/Ei)
+            ))
+        ), Tuples),
+    (member(failed(_/Ei),Tuples) -> (
+        assert( failed(ForID,CID,forall(A,B))),
+        assert( failed_success(ForID,Ei)), 
+        fail
+    ) ; (
+        findall(Ui,member(Ui/_,Tuples),U_), append(U_,U),
+        findall(Ei,member(_/Ei,Tuples),Children_), append(Children_,Children)
+    )).
+i(setof(X,G,L),M,CID,U,E) :- !, E=[w(setof(X,G,L),Children)],
+    wrapTemplateGoal(G,M,CID,Ui,Ei,Wrapped), %TODO: should we introduce an explicit failed node for aggregates?
     setof(X/Ui/Ei, Wrapped, Tuples),
     squeezeTuples(Tuples,L,U,Children).
-i(bagof(X,G,L),M,U,[w(bagof(X,G,L),Children)]) :- !, 
-    wrapTemplateGoal(G,M,Ui,Ei,Wrapped),
+i(bagof(X,G,L),M,CID,U,E) :- !, E=[w(bagof(X,G,L),Children)],
+    wrapTemplateGoal(G,M,CID,Ui,Ei,Wrapped),
     bagof(X/Ui/Ei, Wrapped, Tuples),
     squeezeTuples(Tuples,L,U,Children).
-i(aggregate(Template,G,Result),M,U,[w(aggregate(Template,G,Result),Children)]) :- !, 
+i(aggregate(Template,G,Result),M,CID,U,E) :- !, E=[w(aggregate(Template,G,Result),Children)],
     % uses a bit too much of SWI internals at swipl-devel/library/aggregate.pl
     aggregate:template_to_pattern(bag, Template, Pattern, G, Goal, Aggregate),
-    i(bagof(Pattern, Goal, List),M,U,[w(_Bagof,Children)]),
+    i(bagof(Pattern, Goal, List),M,CID,U,[w(_Bagof,Children)]),
     aggregate:aggregate_list(Aggregate, List, Result).
-i(findall(X,G,L),M,U,[w(findall(X,G,L),Children)]) :- !, 
-    findall(X/Ui/Ei, i(G,M,Ui,Ei), Tuples), 
+i(findall(X,G,L),M,CID,U,E) :- !, E=[w(findall(X,G,L),Children)],
+    findall(X/Ui/Ei, i(G,M,CID,Ui,Ei), Tuples), 
     squeezeTuples(Tuples,L,U,Children).
-i(Q,M,[at(Q,M)],[w(unknown(at(Q,M)))]) :- functor(Q,question,N), (N=1;N=2), !.
-i(At,_Mod,U,E) :- At=at(G,M_), !, % this may cause loading of the module
+i(Q,M,_CID,U,E) :- functor(Q,question,N), (N=1;N=2), !, U=[at(Q,M)], E=[w(unknown(at(Q,M)))].
+i(At,_Mod,CID,U,E) :- At=at(G,M_), !, % this may cause loading of the module
     atom_string(M,M_),
-    (call_at(true,M) -> i(G,M,U,E) ; ( U=[At],E=[w(unknown(At),[])] )).
-i(M:G,_,U,E) :- !, i(G,M,U,E).
-i(G,M,U,E) :- system_predicate(G), !, 
-    evalArgExpressions(G,M,NewG,Uargs,E),
+    (call_at(true,M) -> i(G,M,CID,U,E) ; ( U=[At],E=[w(unknown(At),[])] )).
+i(M:G,_,CID,U,E) :- !, i(G,M,CID,U,E).
+i(G,M,CID,U,E) :- system_predicate(G), !, 
+    evalArgExpressions(G,M,NewG,CID,Uargs,E),
     % floundering originates unknown:
     catch(M:NewG, error(instantiation_error,_Cx), U_=[at(instantiation_error(G),M)]), 
     (var(U_)->U_=[];true),
     append(Uargs,U_,U).
-i(G,M,U,E) :- unknown(G,M), !, (U=[at(G,M)],E=[ w(unknown(at(G,M)),[]) ]).
+i(G,M,_CID,U,E) :- unknown(G,M), !, (U=[at(G,M)],E=[ w(unknown(at(G,M)),[]) ]).
 %TODO: on(G,2020) means "G true on some instant in 2020"; who matches that with '20210107' ?
-i(G,M,U,E) :- 
-    evalArgExpressions(G,M,NewG,Uargs,Eargs),
-    myClause(G,M,B,IsProlog),
-    (IsProlog==false -> i(B,M,U_,Children_) ; (
+i(G,M,CID,U,E) :- 
+    newGoalID(NewID), create_counter(Counter),
+    (true ;( % before failing, save our failure information if no solutions were found
+        get_counter(Counter,0),
+        \+ catch(M:irrelevant_explanation(G),_,fail),
+        assert( failed(NewID,CID,G)),
+        fail
+        )),
+    evalArgExpressions(G,M,NewG,CID,Uargs,Eargs), % failures in the expression (which would be weird btw...) stay directly under CID
+    myClause(G,M,B,_Ref,IsProlog),
+    (IsProlog==false -> i(B,M,NewID,U_,Children_) ; (
         catch(B,error(Error,_),U_=[at(Error,M)]),
         (var(U_)->U_=[];true),
         Children_=[]
     )),
-    
+    inc_counter(Counter), % one more solution found; this is a nonbacktrackable operation
     append(Uargs,U_,U),
     (catch(M:irrelevant_explanation(NewG),_,fail) -> E=Eargs ; (E=[w(G,Children)], append(Eargs,Children_,Children) )).
 
+:- thread_local last_goal_id/1, failed/3, failed_success/2.
 
-evalArgExpressions(G,M,NewG,U,E) :- 
+nextGoalID(ID) :- 
+    (last_goal_id(Old) -> true ; Old=0), ID is Old+1.
+newGoalID(ID) :- 
+    (retract(last_goal_id(Old)) -> true ; Old=0), ID is Old+1, assert(last_goal_id(ID)).
+
+% destructive counters, may also be used as destructive variables that do not lose their value during repeat/fail loops
+create_counter(counter(0)).
+get_counter(counter(N),N).
+set_counter(Counter,N) :- Counter=counter(_), nb_setarg(1,Counter,N).
+inc_counter(Counter,N) :- get_counter(Counter,N), NewN is N+1, nb_setarg(1,Counter,NewN).
+inc_counter(Counter) :- inc_counter(Counter,_).
+
+% Why: w(GoalID,ClauseID,SolutionTerm,Children)
+%storeFailure() : failed(ID,CallerID,CallTerm)
+%storeSuccess() : succeeded(CallerGoalID,Why)
+
+evalArgExpressions(G,M,NewG,CID,U,E) :- 
     G=..[F|Args], 
-    maplist(evalExpression(M),Args,Results,Us,Es),
+    maplist(evalExpression(M,CID),Args,Results,Us,Es),
     NewG=..[F|Results], 
     append(Us,U), append(Es,E).
 
-% evalExpression(+Module,+Expression,-Result,-Unknowns,-WhyExplanation) expands (only) user functions
+% evalExpression(+Module,+Expression,-Result,+CallerID,-Unknowns,-WhyExplanation) expands (only) user functions
 % TODO: add arithmetic expressions too...?
-evalExpression(_M,X,X,[],[]) :- var(X), !.
-evalExpression(M,Exp,R,U,[w(function(Exp),Children)]) :- M:clause(function(Exp,R),Body), !,
-    once( i(Body,M,U,Children) ).
-evalExpression(_M,X,X,[],[]).
+evalExpression(_M,_CID,X,X,[],[]) :- var(X), !.
+evalExpression(M,CID,Exp,R,U,[w(function(Exp),Children)]) :- M:clause(function(Exp,R),Body), !,
+    once( i(Body,M,CID,U,Children) ).
+evalExpression(_M,_CID,X,X,[],[]).
 
-%wrapTemplateGoal(+Gtemplate,+Module,+Unknowns,+Explanation,-WrappedGtemplate)
-% e.g. X^Y^g --> i(X^Y^i(g,Module,Unknowns,Explanation))
-wrapTemplateGoal(G,M,U,E,i(G,M,U,E)) :- var(G), !.
-wrapTemplateGoal(V^G,M,U,E,V^Wrapped) :- !, wrapTemplateGoal(G,M,U,E,Wrapped).
-wrapTemplateGoal(G,M,U,E,i(G,M,U,E)).
+%wrapTemplateGoal(+Gtemplate,+Module,+CallerID,+Unknowns,+Explanation,-WrappedGtemplate)
+% e.g. X^Y^g --> i(X^Y^i(g,Module,CID,Unknowns,Explanation))
+wrapTemplateGoal(G,M,CID,U,E,i(G,M,CID,U,E)) :- var(G), !.
+wrapTemplateGoal(V^G,M,CID,U,E,V^Wrapped) :- !, wrapTemplateGoal(G,M,CID,U,E,Wrapped).
+wrapTemplateGoal(G,M,CID,U,E,i(G,M,CID,U,E)).
 
 %squeezeTuples(+Tuples,-ResultsList,-Unknowns,-Explanations)
 squeezeTuples(Tuples,L,U,Es) :-
@@ -114,19 +183,19 @@ squeezeTuples(Tuples,L,U,Es) :-
     findall(Ei, member(_/_/Ei,Tuples), Es_), append(Es_,Es).
 
 % myClause(+Head,+Module,-Body,-IsProlog)  IsProlog is true if the body should be called directly, without interpretation
-myClause(on(H,Time),M,Body,IsProlog) :- !, myClause2(H,Time,M,Body,IsProlog).
-myClause(H,M,Body,IsProlog) :- myClause2(H,_Time,M,Body,IsProlog).
+myClause(on(H,Time),M,Body,Ref,IsProlog) :- !, myClause2(H,Time,M,Body,Ref,IsProlog).
+myClause(H,M,Body,Ref,IsProlog) :- myClause2(H,_Time,M,Body,Ref,IsProlog).
 
-myClause2(H,Time,M,Body,IsProlog) :- 
-    M:clause(H,Body_), 
+myClause2(H,Time,M,Body,Ref,IsProlog) :- 
+    M:clause(H,Body_,Ref), 
     (Body_=because(on(Body,_Time),_Why) -> IsProlog=true; 
         Body_=on(Body,Time) -> IsProlog=false ; 
         (Body_=Body,IsProlog=false)).
 
 % unknown(+Goal,+Module) whether the knowledge source is currently unable to provide a result 
 unknown(G,M) :- var(G), !, throw(variable_unknown_call_at(M)).
-unknown(on(G,_Time),M) :- !, functor(G,F,N),functor(GG,F,N), \+ myClause2(GG,_,M,_,_).
-unknown(G,M) :- functor(G,F,N),functor(GG,F,N), \+ myClause2(GG,_,M,_,_).
+unknown(on(G,_Time),M) :- !, functor(G,F,N),functor(GG,F,N), \+ myClause2(GG,_,M,_,_,_).
+unknown(G,M) :- functor(G,F,N),functor(GG,F,N), \+ myClause2(GG,_,M,_,_,_).
 
 :- multifile prolog:meta_goal/2. % for xref
 prolog:meta_goal(at(G,M),[M_:G]) :- nonvar(M), atom_string(M_,M).
