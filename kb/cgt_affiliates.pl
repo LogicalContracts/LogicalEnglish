507b09fd6917a491ddd592c0196baf73487253af
jacintodavila <>
2021-04-30 14:07:49 -0400

    refactoring cgt_affiliates. Some basic redefining at the beginning

--- kb/cgt_affiliates.pl
+++ kb/cgt_affiliates.pl
@@ -6,44 +6,49 @@
 
 
 % indicate future API entry point: predicate pattern, description
-mainGoal(has_affiliate(_Entity,_Affiliate),"Determine if a given entity is affiliate of another (also given)").
+mainGoal(has_affiliated_with(_Entity,_Affiliate),"Determine if a given entity is affiliate of another (also given)").
+
+must_not_be_a_variable(Term) :-
+    must_be(nonvar, Term).
+
+is_before(T1, T2) :- T1 before T2. 
 
 example(test,[scenario([
     acts_in_accordance_with_directions_from(company,andrew), 
-    is_trust(_) if false, 
-    is_partnership(_) if false, 
-    is_superannuation_fund(_) if false, 
-    is_individual_or_company(_)
+    is_a_trust(_) if false, 
+    is_a_partnership(_) if false, 
+    is_a_superannuation_fund(_) if false, 
+    is_an_individual_or_a_company(_)
     ],true)]).
 % Assumptions: 
 %   datetimes in iso_8601 format
 
 % note: referred by cgt_concessions_basic_conditions_sb.pl:
-has_affiliate(Entity,Affiliate) on Date if 
+has_affiliated_with(Entity,Affiliate) on Date if 
     Date is_not_before '20090101' % date (?) when affiliate definition changed
-    and is_individual_or_company(Affiliate) 
-	and not is_trust(Affiliate) and not is_partnership(Affiliate) and not is_superannuation_fund(Affiliate)
+    and is_an_individual_or_a_company(Affiliate) 
+	and not is_a_trust(Affiliate) and not is_a_partnership(Affiliate) and not is_a_superannuation_fund(Affiliate)
     and (acts_in_accordance_with_directions_from(Affiliate,Entity) or acts_in_concert_with(Affiliate,Entity)). % seems human-bound!
-has_affiliate(Entity,Affiliate) on Date if
-    Date before '20090101' % date when affiliate definition changed
-    and must_be(nonvar,Entity) and must_be(nonvar,Affiliate)
-    and affiliate_per_older_legislation(Affiliate,Entity).
+has_affiliated_with(Entity,Affiliate) on Date if
+    is_before(Date, '20090101') % date when affiliate definition changed
+    and must_not_be_a_variable(Entity) and must_not_be_a_variable(Affiliate)
+    and are_affiliated_per_older_legislation(Affiliate,Entity).
 
-question( affiliate_per_older_legislation(Affiliate,Entity), "Is '~w' an affiliate of '~w' as per the older legislation" - [Affiliate,Entity]).
+question( are_affiliated_per_older_legislation(Affiliate,Entity), "Is '~w' an affiliate of '~w' as per the older legislation" - [Affiliate,Entity]).
 
 % stub for an "external" Prolog call
-is_individual_or_company(Affiliate) on Date because 'according to myDB_entities' :- 
+is_an_individual_or_a_company(Affiliate) on Date because 'according to myDB_entities' :- 
     myDB_entities:is_individual_or_company_on(Affiliate,Date).
 
-is_trust(Entity) if 
-	is_trust(Entity) at entitiesDB.
-is_partnership(Entity) if
-	is_partnership(Entity) at entitiesDB.
-is_superannuation_fund(Entity) if
-	is_superannuation_fund(Entity) at entitiesDB.
+is_a_trust(Entity) if 
+	is_a_trust(Entity) at entitiesDB.
+is_a_partnership(Entity) if
+	is_a_partnership(Entity) at entitiesDB.
+is_a_superannuation_fund(Entity) if
+	is_a_superannuation_fund(Entity) at entitiesDB.
 
 /** <examples> 
-?- query_with_facts(has_affiliate(andrew,company) on '20200101',test,Unknowns,Explanation,Result).
-?- query_with_facts(has_affiliate(andrew,company) on '20200101',[acts_in_accordance_with_directions_from(company,andrew)],Unknowns,Explanation,Result).
+?- query_with_facts(has_affiliated_with(andrew,company) on '20200101',test,Unknowns,Explanation,Result).
+?- query_with_facts(has_affiliated_with(andrew,company) on '20200101',[acts_in_accordance_with_directions_from(company,andrew)],Unknowns,Explanation,Result).
 ?- le(LogicalEnglish).
 */
