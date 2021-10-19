1b511274c1393e6948c5a03d16f8e5a17cce44a9
jacintodavila <>
2021-10-18 21:18:01 -0400

    Fixing bug in translation of . Extending definition of before/2 to support simple numbers (not only dates). Adjusting answer output not to translate any number below 100 to standard format for dates.

--- kb/014_escrow.pl
+++ kb/014_escrow.pl
@@ -3,59 +3,73 @@
 en("the target language is: prolog. % other languages available soon
 
 the templates are:
-  *an agent* has obtained *a fee*.
+  *an agent* has payed itself *a fee*.
 
 the event predicates are:
-	*a payer* sets up *an escrow* for *an agent* to pay *an amount* to *a payee* and deduce *a fee*.
+	*a payer* sets up *an escrow* for *an agent* to pay *an amount* to *a payee* and deduct *a fee*.
 	*an agent* pays out *an amount* of *an escrow* to *a payee*.
+	*an agent* pays back *an amount* of *an escrow* to *a payer*.
 
 the fluents are:
-	*a payer* has *an escrow* for *an agent* to pay *an amount* to *a payee* and deduce *a fee*.
+	*a payer* has *an escrow* for *an agent* to pay *an amount* to *a payee* and deduct *a fee*.
 	*an escrow* of *an amount* has been paid out to *a payee*.
 
 the knowledge base escrow includes:
 
 it becomes the case that
-	a payer has an escrow for an agent to pay an amount to a payee and deduce a fee
+	a payer has an escrow for an agent to pay an amount to a payee and deduct a fee
 when
-	the payer sets up the escrow for the agent to pay the amount to the payee and deduce the fee.
+	the payer sets up the escrow for the agent to pay the amount to the payee and deduct the fee.
 
 it becomes the case that
 	an escrow of an amount has been paid out to a payee
 when
 	an agent pays out the amount of the escrow to the payee
-if a payer has the escrow for the agent to pay a sum to the payee and deduce a fee
-and the agent has obtained the fee
+if a payer has the escrow for the agent to pay a sum to the payee and deduct a fee
+and the agent has payed itself the fee
 and the amount is the sum - the fee.
 
 it becomes not the case that
-	a payer has an escrow for an agent to pay a sum to a payee and deduce a fee
+	a payer has an escrow for an agent to pay a sum to a payee and deduct a fee
 when
 	the agent pays out an amount of the escrow to the payee
-if the payer has the escrow for the agent to pay the sum to the payee and deduce the fee
-and the agent has obtained the fee
+if the payer has the escrow for the agent to pay the sum to the payee and deduct the fee
+and the agent has payed itself the fee
 and the amount is the sum - the fee.
 
-scenario one is:
-  the payer sets up the escrow for the agent to pay 500 to the payee and deduce 5.
-  the agent has obtained a fee.
+it becomes not the case that
+	a payer has an escrow for an agent to pay a sum to a payee and deduct a fee
+when
+	the agent pays back an amount of the escrow to the payer
+if the payer has the escrow for the agent to pay the sum to the payee and deduct the fee
+and the agent has payed itself the fee
+and the amount is the sum - the fee.    
     
-scenario two is:
-  the payer sets up the escrow for the agent to pay 500 to the payee and deduce 5.
-  the agent has obtained each fee.
-  the agent pays out 495 of the escrow to the payee.
+% initial act  
+    
+the payer sets up the escrow for the agent to pay 500 to the payee and deduct 5, at 1.     
+    
+scenario clause one is:
+  the agent has payed itself each fee. 
+  the agent pays out 495 of the escrow to the payee, at 5. 
+    
+scenario clause two is:
+  the agent has payed itself each fee. 
+  the agent pays back 495 of the escrow to the payee, at 5. 
    
-scenario three is:
-  the payer sets up the escrow for the agent to pay 500 to the payee and deduce 5, at 2021-10-15.
-  the agent has obtained each fee.
-  the agent pays out 495 of the escrow to the payee, at 2021-10-17.
-
 query one is:
-    a payer has an escrow for an agent to pay an amount to a payee and deduce a fee.
+    a payer has an escrow for an agent to pay an amount to a payee and deduct a fee, at 2. 
+
+query two is:
+    a payer has an escrow for an agent to pay an amount to a payee and deduct a fee, at 8. 
+    
+query three is:
+    which agent pays back which amount of which escrow to which payee. 
 
 ").
 
 /** <examples>
-?- answer("query one with scenario one").
-?- answer("query one").
+?- answer("query one with scenario clause one").
+?- answer("query one with scenario clause two").
+?- answer("query three with scenario clause two").
 */
