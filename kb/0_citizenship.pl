ea098a48edf9eeb757981e4737eded85070a0842
jacintodavila <>
2021-11-14 20:52:29 -0400

    Adding the BNC as a separated example with proper module name

---
+++ kb/0_citizenship.pl
@@ -0,0 +1,34 @@
+:- module('citizenship+https://www.legislation.gov.uk/ukpga/1981/61/body/',[]).
+
+en("the target language is: prolog.
+
+the templates are:
+*a person* acquires British citizenship on *a date*.
+*a person* is born in *a place* on *a date*,
+*a date* is after commencement,
+*a person* is the mother of *a person*,
+*a person* is the father of *a person*,
+*a person* is a British citizen on *a date*,
+*a person* is settled in the UK on *a date*,
+*a person* says that *a sentence*,
+*a person* is authorised to determine fatherhood.
+
+the knowledge base citizenship includes:
+a person acquires British citizenship on a date
+if the person is born in the UK on the date
+and the date is after commencement
+and an other person is the mother of the person
+    or an other person is the father of the person
+and the other person is a British citizen on the date
+    or the other person is settled in the UK on the date.
+
+scenario alice is:
+John is born in the UK on 2021-10-09.
+2021-10-09 is after commencement.
+Alice is the mother of John.
+Alice is a British citizen on 2021-10-09.
+        
+query one is:
+    
+which person acquires British citizenship on which date.
+").
