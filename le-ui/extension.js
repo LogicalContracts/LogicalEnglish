8c1a2392f206497ccce196397f3de302e454b92e
jacintodavila <>
2022-10-29 22:33:16 -0400

    Testing showInputBox in le-ui

--- le-ui/extension.js
+++ le-ui/extension.js
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+//import vscode from 'vscode';
+
+console.log(vscode.window)
+
+let myStatusBarItem; 
+let leQuery = '';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -34,10 +40,61 @@ function activate(context) {
 	});
 
 	context.subscriptions.push(newcommand);
+
+	let newInputcommand = vscode.commands.registerCommand('le-ui.query', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInputBox().then((value) => {leQuery=value})
+		console.log('Querying', leQuery)
+	});
+
+	context.subscriptions.push(newInputcommand);
+
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'le-ui.showSelectionCount';
+	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+		console.log(`Yeah, ${n} line(s) selected... Keep going!`);
+		vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem('LE', vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	context.subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	updateStatusBarItem();
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+function updateStatusBarItem() {
+	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+	if (n > 0) {
+		myStatusBarItem.text = `LE$(megaphone) ${n} line(s) selected`;
+		myStatusBarItem.show();
+	} else {
+		myStatusBarItem.hide();
+	}
+}
+
+function getNumberOfSelectedLines(editor) {
+	let lines = 0;
+	if (editor) {
+		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+	}
+	return lines;
+}
+
 
 module.exports = {
 	activate,
