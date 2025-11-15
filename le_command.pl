8bb74adc6b08435f5a50788c7e49f5fb5351d52d
Miguel Calejo <mc@interprolog.com>
2025-11-15 18:01:16 +0000

    le_command tweak

--- le_command.pl
+++ le_command.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/env swipl
 
-% For example /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=verify /Users/mc/git/LogicalEnglish/moreExamples/citizenship.le
-% cd ~/git/LogicalEnglish ; /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=query --query="which person is the mother of which other person." --scenario="Clara is the mother of Miguel." /Users/mc/git/LogicalEnglish/moreExamples/citizenship.le
-% --command=verify/load/query
+% For example 
+% /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=list /Users/mc/git/LogicalEnglish/moreExamples
+% /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=verify /Users/mc/git/LogicalEnglish/moreExamples/citizenship.le
+% /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=query --query="which person is the mother of which other person." --scenario="Clara is the mother of Miguel." /Users/mc/git/LogicalEnglish/moreExamples/citizenship.le
+% cd ~/git/LogicalEnglish ; /Applications/SWI-Prolog9.3.33-1.app/Contents/MacOS/swipl le_command.pl --command=query --query="which person acquires British citizenship on which date." --scenario="John is born in the UK on 2021-10-09. 2021-10-09 is after commencement. Harry is the father of John. Harry is settled in the UK on 2021-10-09." /Users/mc/git/LogicalEnglish/moreExamples/citizenship.le
+% --command=list/verify/load/query
 % Useful to obtain error messages from loading and/or verifying expectations (test results) for a program, as well as straight querying a LE program
 :- use_module(le_cli).
 
@@ -39,6 +42,8 @@ main(Argv) :-
     ( memberchk(command(Command), Opts) ->  
         (Command == load -> load_program(File) ; 
             Command == verify -> verify_expectations(File) ;
+            Command == list -> print_programs_and_main_predicates(File) ;
+            Command == mcp -> mcpLoop(File) ;
             Command == query -> 
                 memberchk(query(Q), Opts), memberchk(scenario(S), Opts),
                 load_and_query_program_all(File,S,Q,AnswerExplanations,_Answers,Sentences),
@@ -53,3 +58,7 @@ main(Argv) :-
         format('No command specified for file ~w~n', [File])
     ),
     halt(0).
+
+mcpLoop(Dir) :-
+    introspect_programs_and_main_predicates(Dir,Pairs),
+    writeln(Pairs).
